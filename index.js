503123f2bed6d2632b7aaaa9d07ad12e1b617c11
Patricia <142860180+patmcd4@users.noreply.github.com>
2023-11-27 19:41:50 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+// tic tac toe
+
+//set up board
+
+    //track current player turn (X or O)
+
+    //track how many plays have occured (in case of tie) (play count)
+
+    // create 9 boxes for board and append to the board element 
+        // boxes need click listener     
+            // check if box is empty or taken
+            // add the value for current player (X or O)
+            // possibly remove click listener
+            // check for win
+            // if no win check gamecount is less than 9
+            // update the current player
+            // update a message on the screen so you know who's turn it is  
+//
+
+//take turns
+    //
+//
+
+// check for win (only necessary when playcount hits 5)
+
+    // if someone wins message
+
+    // if not win, switch player
+//
+
+
+
+function startGame() {}
+
+function createBoxes() {}
+
+function checkforWin() {}
+
+
